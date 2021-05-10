051350de9c51335704439f6ff397abf05baf176f
Avinash Kumar <itsmeatg@outlook.com>
2021-05-10 15:12:08 +0530

    Adding Routing feature with Login,register & Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div>
-      <p>React App</p>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/egister" component ={Register}/>  
+    </Switch>
   );
 }
 
