19a8d675ae28c32ce617f7863526822f198d8060
Avinash Kumar <itsmeatg@outlook.com>
2021-05-11 14:12:01 +0530

    Adding Complete Registration page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
+import {ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
+import Header from './component/nav/header';
+import RegisterComplete from './pages/auth/RegisterComplete';
+
 
 
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/register" component ={Register}/>  
-    </Switch>
+    <div>
+      <Header/>
+      <ToastContainer/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component ={Register}/>  
+        <Route exact path="/register/complete" component ={RegisterComplete}/>
+      </Switch>
+    </div>
   );
 }
 
--- client/src/component/nav/header.js
+++ client/src/component/nav/header.js
@@ -1,48 +1,42 @@
-import React from 'react';
-import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import React, { useState } from "react";
+import { Menu } from "antd";
+import {
+  AppstoreOutlined,
+  SettingOutlined,
+  UserOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
 
-const { SubMenu } = Menu;
+const { SubMenu, Item } = Menu;
 
-class Header extends React.Component {
-  state = {
-    current: 'mail',
+const Header = () => {
+  const [current, setCurrent] = useState("home");
+
+  const handleClick = (e) => {
+    // console.log(e.key);
+    setCurrent(e.key);
   };
 
-  handleClick = e => {
-    console.log('click ', e);
-    this.setState({ current: e.key });
-  };
+  return (
+    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+      <Item key="home" icon={<AppstoreOutlined />}>
+        Home
+      </Item>
 
-  render() {
-    const { current } = this.state;
-    return (
-      <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
-        <Menu.Item key="mail" icon={<MailOutlined />}>
-          Navigation One
-        </Menu.Item>
-        <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-          Navigation Two
-        </Menu.Item>
-        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
-          <Menu.ItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </Menu.ItemGroup>
-          <Menu.ItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </Menu.ItemGroup>
-        </SubMenu>
-        <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-            Navigation Four - Link
-          </a>
-        </Menu.Item>
-      </Menu>
-    );
-  }
-}
+      <Item key="register" icon={<UserAddOutlined />} className="float-right">
+        Register
+      </Item>
+
+      <Item key="login" icon={<UserOutlined />} className="float-right">
+        Login
+      </Item>
+
+      <SubMenu icon={<SettingOutlined />} title="Username">
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
+      </SubMenu>
+    </Menu>
+  );
+};
 
 export default Header;
-
