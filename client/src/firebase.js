2a2835d4e134bd6b4bb7509588e65c7799752ba9
Avinash Kumar <itsmeatg@outlook.com>
2021-05-11 13:16:14 +0530

    Adding Package Toastify and Completing Email Sending

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,9 +1,10 @@
-import * as firebase from 'firebase';
+import firebase from "firebase/app";
+import "firebase/auth";
 
 
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
+var config = {
     apiKey: "AIzaSyBlS_Sl1e6MfH3GNTYzQBokI5WWXVpGyjU",
     authDomain: "ecommerce-ad307.firebaseapp.com",
     projectId: "ecommerce-ad307",
@@ -11,10 +12,12 @@ var firebaseConfig = {
     messagingSenderId: "57056194843",
     appId: "1:57056194843:web:048e8d491ec737489be3be"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-export const auth = firebase.auth();
-export const googleAuthProvider=new firebase.auth.googleAuthProvider();
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  }
+  // export
+  // export default firebase;
+  export const auth = firebase.auth();
+  export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 
