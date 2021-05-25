1b82f9a532cab46a19460e7aedaab3119b393352
Anjali590 <crazywarrior529@gmail.com>
2021-05-25 12:38:04 +0530

    Adding  Secton 5

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,16 @@
+
 const express = require("express");
 
 const router = express.Router();
 
+// middlewares
+
+const { authCheck } = require("../middlewares/auth");
+
+
 // import
 const { createOrUpdateUser } = require("../controllers/auth");
 
-router.get("/create-or-update-user", createOrUpdateUser);
+router.post("/create-or-update-user", authCheck,createOrUpdateUser);
 
 module.exports = router;
